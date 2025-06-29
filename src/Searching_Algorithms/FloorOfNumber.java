422b8b6b645e2f74b54607442241c4c2c62df6cf
jasmanCodes <jasman120105@gmail.com>
2025-06-29 10:07:10 +0530

    added code of binary search

---
+++ src/Searching_Algorithms/FloorOfNumber.java
@@ -0,0 +1,45 @@
+package Searching_Algorithms;
+
+public class FloorOfNumber {
+    public static void main(String[] args) {
+
+        int[] arr= {2,3,4,5,9,14,16,18};
+        int target = 12;
+
+        int ans= floor(arr,target);
+        System.out.println(ans);
+
+    }
+
+    static int floor(int[] arr, int target){
+
+        if(target<arr[arr.length-1]){
+            return -1;
+        }
+
+        int start=0;
+        int end= arr.length-1;
+
+        while(start<=end){
+
+            int mid=start+(end-start)/2;
+
+
+            if(target>arr[mid]){
+                start= mid+1;
+            }
+            else if(target<arr[mid]){
+                end=mid-1;
+            }
+            else{
+                return arr[mid];
+            }
+
+        }
+        return arr[end];
+    }
+
+
+}
+
+
