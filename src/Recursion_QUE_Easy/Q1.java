95f083e667c053702a2591ed0a94c9b38086eee5
jasmanCodes <jasman120105@gmail.com>
2025-07-23 02:01:23 +0530

    recursion practice

--- src/Recursion_QUE_Easy/Q1.java
+++ src/Recursion_QUE_Easy/Q1.java
@@ -4,7 +4,7 @@ public class Q1 {
 
     public static void main(String[] args) {
 
-        display(5);
+        displayboth(5);
 
     }
 
@@ -14,6 +14,29 @@ public class Q1 {
         }
         System.out.println(n);
         display(n-1);
+
+
+    }
+//printing 1 to n
+    static void displayrev(int n){
+        if(n==0){
+            return;
+        }
+
+        displayrev(n-1);
+        System.out.println(n);
+
+
+    }
+
+    //printing 1 to n and n to 1
+    static void displayboth(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        displayrev(n-1);
+        System.out.println(n);
 
 
     }
