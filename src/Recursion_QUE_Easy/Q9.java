5ffa11a086305d9644da3acd154792b7c04e27b3
jasmanCodes <jasman120105@gmail.com>
2025-07-24 02:13:52 +0530

    recursion practice

--- src/Recursion_QUE_Easy/Q9.java
+++ src/Recursion_QUE_Easy/Q9.java
@@ -1,11 +1,16 @@
 package Recursion_QUE_Easy;
- /// linear search searchin =g for a target in an array
+
+import java.util.ArrayList;
+
+/// linear search searchin =g for a target in an array
 public class Q9 {
 
      public static void main(String[] args) {
 
-         int[] arr = {5,67,4,1,42,12,90};
+         int[] arr = {5,67,4,1,42,4,90};
          System.out.println(find(arr,12,0));
+         findallindex(arr,4,0);
+         System.out.println(list);
 
      }
 
@@ -15,5 +20,19 @@ public class Q9 {
         }
 
          return arr[index]==target || find(arr,target,index+1);
+     }
+
+
+     ///  finding all ocurrences
+     static ArrayList<Integer> list= new ArrayList<>();
+     static void findallindex(int[] arr, int target, int index){
+         if(index==arr.length){
+             return;
+         }
+
+         if(arr[index]==target){
+             list.add(index);
+         }
+         findallindex(arr,target,index+1);
      }
 }
