5aa36118e37e8f9ef4aefff62c260b65f327f6c1
jasmanCodes <jasman120105@gmail.com>
2025-07-26 17:18:52 +0530

    string

---
+++ src/Strings/Ascii.java
@@ -0,0 +1,8 @@
+package Strings;
+
+public class Ascii {
+    public static void main(String[] args) {
+         char ch = 'a';
+        System.out.println(ch+0);  //this gives the ascii values of a alphabet
+    }
+}
