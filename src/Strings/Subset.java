07fa76522e5b4eade415f3d6661c34e6619c3ecf
jasmanCodes <jasman120105@gmail.com>
2025-07-26 18:34:13 +0530

    codes added

--- src/Strings/Subset.java
+++ src/Strings/Subset.java
@@ -2,13 +2,14 @@ package Strings;
 //this is using iteration
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
 public class Subset {
 
     public static void main(String[] args) {
 
-        int[] arr = {1,2,3};
-        List<List<Integer>> ans = subset(arr);
+        int[] arr = {1,2,2};
+        List<List<Integer>> ans = subsetDuplicate(arr);
         System.out.println(ans);
 
 
@@ -26,6 +27,40 @@ public class Subset {
                 List<Integer> internal=new ArrayList<>(outer.get(i));
 
                 internal.add(num);
+                outer.add(internal);
+
+            }
+
+        }
+
+        return outer;
+
+    }
+
+
+    static List<List<Integer>> subsetDuplicate(int[] arr) {
+
+        Arrays.sort(arr);
+
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        int start = 0;
+        int end = 0;
+
+        for(int i = 0; i<arr.length; i++){
+            start=0;
+            //if my current and previous element is same , s= e+1
+            if(i>0 && arr[i] == arr[i-1]){
+
+                start = end+1;
+            }
+            end = outer.size()-1;
+            int n = outer.size();
+
+            for(int j=start;j<n;j++){
+                List<Integer> internal=new ArrayList<>(outer.get(j));
+
+                internal.add(arr[i]);
                 outer.add(internal);
 
             }
