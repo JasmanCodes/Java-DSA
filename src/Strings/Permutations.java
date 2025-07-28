20cc2fc740a22ae14300b545c0733afa91882227
jasmanCodes <jasman120105@gmail.com>
2025-07-28 21:07:50 +0530

    recursion practice

--- src/Strings/Permutations.java
+++ src/Strings/Permutations.java
@@ -7,8 +7,10 @@ public class Permutations {
     public static void main(String[] args) {
     //permutations("", "abc ");
 
-       ArrayList<String> ans = permutationsList("","abc");
-        System.out.println(ans);
+       //ArrayList<String> ans = permutationsList("","abc");
+        //System.out.println(ans);
+
+        System.out.println(permutationsCount("","abcd"));
     }
 
     static void permutations(String p, String up){
@@ -51,6 +53,27 @@ public class Permutations {
            ans.addAll(permutationsList(f + ch + s, up.substring(1)));
         }
         return ans;
+
+
+    }
+//returning the number of permutations
+    static int permutationsCount(String p, String up){
+
+        if(up.isEmpty()){
+
+            return 1;
+        }
+int count = 0;
+        char ch = up.charAt(0);
+
+        for(int i=0; i<=p.length();i++){
+
+            String f = p.substring(0,i);
+            String s = p.substring(i,p.length());
+
+            count = count+ permutationsCount(f + ch + s, up.substring(1));
+        }
+        return count;
 
 
     }
