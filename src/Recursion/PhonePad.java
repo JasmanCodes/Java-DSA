ecddd86c56c9ca4d6177b16500dba0d048de080a
jasmanCodes <jasman120105@gmail.com>
2025-08-04 23:33:05 +0530

    recursion practice

--- src/Recursion/PhonePad.java
+++ src/Recursion/PhonePad.java
@@ -9,7 +9,10 @@ public class PhonePad {
     public static void main(String[] args) {
 
       //  pad("", "12");
-        System.out.println(padRet("","12 "));
+       // System.out.println(padRet("","12 "));
+        System.out.println(padCount("","12"));
+
+
 
     }
 
@@ -46,6 +49,26 @@ public class PhonePad {
 
         }
         return list;
+
+    }
+
+    static int padCount(String p , String up){
+        if(up.isEmpty()){
+
+            return 1;
+        }
+        int count =0;
+        int digit = up.charAt(0) - '0';  // this will convert '2' into 2
+
+
+
+        for(int i = (digit-1)*3; i<digit *3; i++){
+
+            char ch = (char) ('a'+i);
+            count = count +(padCount(p+ch, up.substring(1)));
+
+        }
+        return count;
 
     }
 }
