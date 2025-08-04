cff74a3decd9f986824503721f0aef9c49d3f043
jasmanCodes <jasman120105@gmail.com>
2025-08-04 23:27:03 +0530

    recursion practice

--- src/Recursion/PhonePad.java
+++ src/Recursion/PhonePad.java
@@ -1,11 +1,15 @@
 package Recursion;
+
+import java.util.ArrayList;
+
 //leetcode 17
 //important google question
 public class PhonePad {
 
     public static void main(String[] args) {
 
-        pad("", "12");
+      //  pad("", "12");
+        System.out.println(padRet("","12 "));
 
     }
 
@@ -25,5 +29,23 @@ public class PhonePad {
 
     }
 
+    static ArrayList<String> padRet(String p , String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        int digit = up.charAt(0) - '0';  // this will convert '2' into 2
 
+        ArrayList<String> list = new ArrayList<>();
+
+        for(int i = (digit-1)*3; i<digit *3; i++){
+
+            char ch = (char) ('a'+i);
+            list.addAll(padRet(p+ch, up.substring(1)));
+
+        }
+        return list;
+
+    }
 }
