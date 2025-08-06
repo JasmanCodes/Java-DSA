f2640b7c09c4dd6b0693e9dee1eacaf6c4d04ca3
jasmanCodes <jasman120105@gmail.com>
2025-08-06 22:31:11 +0530

    backtracking

--- src/Backtracking/Maze.java
+++ src/Backtracking/Maze.java
@@ -10,7 +10,8 @@ public class Maze {
      //   System.out.println(count(3,3));
 
       //  path("",3,3);
-        System.out.println(pathRet("",3,3));
+      //  System.out.println(pathRet("",3,3));
+        System.out.println(pathRetDiagonal("",3,3));
 
     }
 
@@ -59,4 +60,29 @@ public class Maze {
             }
             return list;
         }
+
+//now we can go in diaognal also
+    static ArrayList<String> pathRetDiagonal(String p,int r, int c){
+        if (r == 1 && c == 1) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+
+        if(r>1 && c>1){
+            list.addAll(pathRetDiagonal(p+'D',r-1,c-1));
+        }
+
+
+        if (r > 1) {
+            list.addAll(pathRetDiagonal(p+'V',r-1,c));
+        }
+        if (c > 1) {
+            list.addAll(pathRetDiagonal(p + 'H', r, c - 1));
+
+        }
+        return list;
+    }
     }
