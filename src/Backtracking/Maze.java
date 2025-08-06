56267cb29bc88e7b538f2ca664552f9fa048dbca
jasmanCodes <jasman120105@gmail.com>
2025-08-06 19:05:20 +0530

    backtracking

--- src/Backtracking/Maze.java
+++ src/Backtracking/Maze.java
@@ -1,10 +1,16 @@
 package Backtracking;
+
+import java.util.ArrayList;
+
 //doing with recursion count of paths
 public class Maze {
 
     public static void main(String[] args) {
 
-        System.out.println(count(3,3));
+     //   System.out.println(count(3,3));
+
+      //  path("",3,3);
+        System.out.println(pathRet("",3,3));
 
     }
 
@@ -17,4 +23,40 @@ public class Maze {
 
         return left+right;
     }
-}
+
+    /// returning the path
+    static void path(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            System.out.println(p);
+            return;
+        }
+
+        if (r > 1) {
+            path(p + 'D', r - 1, c);
+        }
+        if (c > 1) {
+            path(p + 'R', r, c - 1);
+
+        }
+    }
+
+//returning the arraylist
+        static ArrayList<String> pathRet(String p,int r, int c){
+            if (r == 1 && c == 1) {
+                ArrayList<String> list = new ArrayList<>();
+                list.add(p);
+                return list;
+            }
+
+            ArrayList<String> list = new ArrayList<>();
+
+            if (r > 1) {
+                list.addAll(pathRet(p + 'D', r - 1, c));
+            }
+            if (c > 1) {
+                list.addAll(pathRet(p + 'R', r, c - 1));
+
+            }
+            return list;
+        }
+    }
