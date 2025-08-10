8f0360a93a13a6481cae94ae1749085d267ec578
jasmanCodes <jasman120105@gmail.com>
2025-08-10 16:39:33 +0530

    backtracking

---
+++ src/Backtracking/SudokoSolver.java
@@ -0,0 +1,94 @@
+package Backtracking;
+
+public class SudokoSolver {
+    public static void main(String[] args) {
+        int[][] board = {
+                {3, 0, 6, 5, 0, 8, 4, 0, 0},
+                {5, 2, 0, 0, 0, 0, 0, 0, 0},
+                {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                {0, 0, 3, 0, 1, 0, 0, 8, 0},
+                {9, 0, 0, 8, 6, 3, 0, 0, 5},
+                {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                {0, 0, 5, 2, 0, 6, 3, 0, 0}
+        };
+
+        if (solve(board)) {
+            display(board);
+        } else {
+            System.out.println("No solution exists");
+        }
+    }
+
+    static boolean solve(int[][] board) {
+        int n = board.length;
+        int row = -1, col = -1;
+        boolean emptyLeft = true;
+
+        // Find empty cell
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 0) {
+                    row = i;
+                    col = j;
+                    emptyLeft = false;
+                    break;
+                }
+            }
+            if (!emptyLeft) break;
+        }
+
+        // No empty cell left → puzzle solved
+        if (emptyLeft) return true;
+
+        // Backtracking
+        for (int number = 1; number <= 9; number++) {
+            if (isSafe(board, row, col, number)) {
+                board[row][col] = number;
+                if (solve(board)) return true; // Solution found
+                board[row][col] = 0; // Backtrack
+            }
+        }
+        return false; // No solution
+    }
+
+    static void display(int[][] board) {
+        for (int[] row : board) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static boolean isSafe(int[][] board, int row, int col, int num) {
+        // Row check
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] == num) {
+                return false;
+            }
+        }
+
+        // Column check
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+        }
+
+        // 3x3 grid check
+        int sqrt = (int) Math.sqrt(board.length);
+        int rowStart = row - row % sqrt;
+        int colStart = col - col % sqrt;
+
+        for (int r = rowStart; r < rowStart + sqrt; r++) {
+            for (int c = colStart; c < colStart + sqrt; c++) {
+                if (board[r][c] == num) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
